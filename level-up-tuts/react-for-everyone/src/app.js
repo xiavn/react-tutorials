40500c5883cda6735c5d2be68d1848eeb2e8e423
Lianne Turnbull <lianneturnbull@gmail.com>
2016-07-08 13:05:51 +0100

    Iterate over an array and pass to child

--- level-up-tuts/react-for-everyone/src/app.js
+++ level-up-tuts/react-for-everyone/src/app.js
@@ -2,15 +2,33 @@ import React from 'react';
 import {render} from 'react-dom';
 import ContactsList from './ContactsList';
 
+let contacts = [{
+	id: 1,
+	name: 'Adam',
+	phone: '555 555 5555'
+}, {
+	id: 2,
+	name: 'Bob',
+	phone: '555 111 5555'
+}, {
+	id: 3,
+	name: 'Claude',
+	phone: '555 555 1234'
+}, {
+	id: 4,
+	name: 'Dave',
+	phone: '444 555 5555'
+}];
+
 class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Contacts List</h1>
-				<ContactsList />
+				<ContactsList contacts={this.props.contacts}/>
 			</div>
 		)
 	}
 }
 
-render(<App />, document.getElementById('app'));
+render(<App contacts={contacts}/>, document.getElementById('app'));
