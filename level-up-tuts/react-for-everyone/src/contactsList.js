97ca3c2ff1a5cacf918a7d37c44370edbd2f0649
Lianne Turnbull <lianneturnbull@gmail.com>
2016-07-08 12:47:23 +0100

    Add a child and import/export

---
+++ level-up-tuts/react-for-everyone/src/contactsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+	render() {
+		return (
+			<ul>
+				<li>Adam 01233 455673</li>
+			</ul>
+		)
+	}
+}
+
+export default ContactsList;
