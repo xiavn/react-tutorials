ec9f9d02f35e3d3b318227c113698e4cc2e9153c
Lianne Turnbull <lianneturnbull@gmail.com>
2016-07-09 10:34:18 +0100

    Add search input

--- level-up-tuts/react-for-everyone/src/contactsList.js
+++ level-up-tuts/react-for-everyone/src/contactsList.js
@@ -2,13 +2,34 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			search: ''
+		};
+	}
+
+	updateSearch(event) {
+		this.setState({search: event.target.value});
+	}
+
 	render() {
+		let filteredContacts = this.props.contacts.filter(
+				(contact) => {
+					return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) != -1;
+				}
+			);
 		return (
-			<ul>
-				{this.props.contacts.map((contact)=> {
-					return <Contact contact={contact} key={contact.id}/>
-				})}
-			</ul>
+			<div>
+				<input type="text" 
+						value={this.state.search} 
+						onChange={this.updateSearch.bind(this)} />
+				<ul>
+					{filteredContacts.map((contact)=> {
+						return <Contact contact={contact} key={contact.id}/>
+					})}
+				</ul>
+			</div>
 		)
 	}
 }
